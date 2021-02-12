f2f74e99f8d8cac9014bd41849b851c4dad1d244
SooHwanCho <chh0107@gmail.com>
2021-02-12 12:01:17 +0900

    Initial Commit

---
+++ TexturePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TexturePractice
+//
+//  Created by Joshua on 2021/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
